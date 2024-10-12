2582080eb645374a77734476636f3345d547f637
kathleen-scafidi <112360246+kathleen-scafidi@users.noreply.github.com>
2024-10-11 20:35:56 -0700

    Added functionality to gesture towards hand.
    To run, open individual terminals for roscore, rviz, nao_sim.launch, the publisher gui, and week4 joint_demo.py. Then control the left arm's position from the gui to see the head gesture toward the hand.

--- week4/scripts/tf_look_at_hand.py
+++ week4/scripts/tf_look_at_hand.py
@@ -1,26 +1,50 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import rospy
-
 import math
 import tf2_ros
-import geometry_msgs.msg
+from sensor_msgs.msg import JointState
 
-# this is based on the ROS tf2 tutorial: http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28Python%29
+def get_hand_position(tfBuffer):
+    try:
+        trans = tfBuffer.lookup_transform('torso', 'l_gripper', rospy.Time())
+        return trans.transform.translation
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        return None
 
-if __name__ == '__main__':
-    rospy.init_node('tf2_look_at_hand')
+def calculate_head_angles(hand_position):
+    yaw = math.atan2(hand_position.y, hand_position.x)
+    pitch = math.atan2(-hand_position.z, math.sqrt(hand_position.x**2 + hand_position.y**2)) #this exagerates the gesture 
+    return yaw, pitch
 
+def gesture_toward_hand():
+    rospy.init_node('nao_gesture_toward_hand')
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
+    pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
 
-
-    rate = rospy.Rate(10.0)
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        try:
-            trans = tfBuffer.lookup_transform('Head', 'l_gripper', rospy.Time())
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rate.sleep()
-            continue
-
-        print("trans: x: %f y: %f z: %f", trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z)
+        hand_position = get_hand_position(tfBuffer)
+        if hand_position:
+            yaw, pitch = calculate_head_angles(hand_position)
+            
+            #head gesture toward hand
+            gesture = JointState()
+            gesture.header.stamp = rospy.Time.now()
+            gesture.name = ['HeadYaw', 'HeadPitch']
+            gesture.position = [yaw, pitch]
+            pub.publish(gesture)
+            rospy.sleep(1)
+            
+            #head back to center
+            gesture.position = [0, 0]
+            pub.publish(gesture)
+        
         rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        gesture_toward_hand()
+    except rospy.ROSInterruptException:
+        pass
+
