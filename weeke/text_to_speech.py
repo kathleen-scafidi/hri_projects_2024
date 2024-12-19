b6352ea82b4daa1b73e30bf37842d60366984789
kathleen-scafidi <112360246+kathleen-scafidi@users.noreply.github.com>
2024-12-18 18:11:23 -0800

    Updated text_to_speech.py

--- weeke/text_to_speech.py
+++ weeke/text_to_speech.py
@@ -3,16 +3,15 @@ import rospy
 from std_msgs.msg import String
 
 def speak_text():
-    # Initialize the ROS node
+   
     rospy.init_node('speaker_node', anonymous=True)
     
-    # Create a publisher to the TTS topic
+    #setup TTS
     pub = rospy.Publisher('/tts/phrase', String, queue_size=10)
     
-    # Wait a moment to ensure the publisher is ready
     rospy.sleep(1)
     
-    # Publish a string to speak
+    #publish string
     pub.publish(data="Hello, this is a test message")
 
 if __name__ == '__main__':
